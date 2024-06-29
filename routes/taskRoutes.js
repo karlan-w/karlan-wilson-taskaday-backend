46c828967f9c0dd5ad6161e3ac6dea37770b8461
Karlan Wilson <karlan@Karlans-MacBook-Pro-885.local>
2024-06-29 16:05:17 -0400

    Added an if statement to share a 404 error in case a users shares a searched date that does not exist in the database. Created put statement. This was tested and I’ve confirmed it changes the information in the tasks table.

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const knex = require('knex')(require('../knexfile'))
 
 //Routes Needed
-//Get tasks for a specific date
-//Put to edit tasks 
+//Put to edit task name 
 //Delete Tasks 
 //Post focus time 
 //Post break time 
@@ -16,25 +15,58 @@ const knex = require('knex')(require('../knexfile'))
 //Update session count (get session amount for task once task is complete) --> Can be done on front end 
 //Update number of sessions (post sessions total)--> Can be done on front end
 
-
+//Get all task information for a specific date 
 router.get('/:date',(req,res)=>{
     const taskDay = req.params.date;
-    knex.select('*').from('tasks').where('date',taskDay)
+
+    knex.select('*')
+    .from('tasks')
+    .where('date',taskDay)
     .then((data)=>{
-        // if(taskDay!== knex.select('date').from('tasks')){
-        //     res.send('Incorrect Date added');
-        // }
-        return res.send(data)
+    //Does the date exist? 
+        if(data.length === 0){
+            res.status(404).send('Date not found.');
+        }
+    //Return array of dates if date exists
+        return res.json(data);
     })
     .catch((err)=>{
         return res.status(500).send(err)
     });
-
-    //Complete migration 
-    //Compelte Seeds
-    //Complete get statement
-    //Continue through statements 
 });
 
+// Edit task name for a specific task
+router.put('/task/:taskId',(req,res)=>{
+    const taskId = req.params.taskId;
+    console.log(taskId);
+    const updatedInfo = req.body;
+    knex('tasks')
+    .where('task_id',taskId)
+    .update(updatedInfo)
+    .then(()=>{
+       return knex.select('*').from('tasks').where('task_id',taskId);
+    }).then((data)=>{
+       return res.status(200).json(data);
+    }).catch((err)=>{
+        res.status(500).send(err);
+    })
+});
+
+//Delete Tasks 
+
+// Post a Task (need to add a if statement if task does not exist) + Add validation for task name (no symbols)
+
+
+//Post focus time 
+
+//Post break time 
+
+//Post forced break
+
+
+//Get total break time for 7 days (Pull current day, calculate last 7 days, send totals for each)
+
+
+//Get total focus time for 7 days
 
 module.exports = router; 
